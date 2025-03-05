0727e26d5203c4e4782a208b0ec15ce69193b1e9
D4nf313 <danfecisf@gmail.com>
2025-03-05 01:15:01 -0500

    add tabla vehiculos registrados

--- src/app/components/list-vehicle/list-vehicle.component.ts
+++ src/app/components/list-vehicle/list-vehicle.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { MatTableModule } from '@angular/material/table'; // Importa MatTableModule
+import { MatPaginatorModule } from '@angular/material/paginator'; // Opcional: para paginación
+import { MatSortModule } from '@angular/material/sort'; // Opcional: para ordenar
+import { VehicleService } from '../../services/vehicle.service';
+import { Vehicle } from '../vehicle-form/vehicle-form.interface';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-list-vehicle',
-  imports: [],
+  imports: [
+    MatTableModule, // Importa MatTableModule
+    MatPaginatorModule, // Opcional: para paginación
+    MatSortModule,
+    MatButtonModule,
+  ],
   templateUrl: './list-vehicle.component.html',
-  styleUrl: './list-vehicle.component.scss'
+  styleUrl: './list-vehicle.component.scss',
 })
-export class ListVehicleComponent {
+export class ListVehicleComponent implements OnInit {
+  displayedColumns: string[] = ['licensePlate', 'entryTime', 'exitTime', 'actions'];
+  dataSource: Vehicle[] = [];
 
+  constructor(private vehicleService: VehicleService) {}
+
+  
+  ngOnInit(): void {
+    this.dataSource = this.vehicleService.getVehicles();
+  }
+
+  darSalida(vehicle: Vehicle): void {
+    // Lógica para dar salida (puede actualizar exitTime con la hora actual)
+    vehicle.exitTime = new Date().toLocaleTimeString(); 
+  }
 }
