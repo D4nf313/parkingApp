98fb726d17169518ac75132ce19d0f03d7aa105b
D4nf313 <danfecisf@gmail.com>
2025-03-04 21:15:00 -0500

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+      path: '',
+      loadComponent: () =>
+        import('./components/dashboard/dashboard.component').then(
+          (m) => m.DashboardComponent
+        ),
+    },
+    {
+      path: 'vehicles',
+      loadComponent: () =>
+        import('./components/list-vehicle/list-vehicle.component').then(
+          (m) => m.ListVehicleComponent
+        ),
+    },
+
+    {
+      path: '**',
+      redirectTo: '',
+    },
+  ];
